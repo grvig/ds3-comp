c37f717f23089e425d02d89b48abd47205976238
GR <grvig06@gmail.com>
2026-06-18 18:45:04 +0530

    Add initial boss tracking data for tests

--- main.lua
+++ main.lua
@@ -1,3 +1,18 @@
+bosses = {
+    {
+        name = "Iudex Gundyr",
+        defeated = false
+    },
+    {
+        name = "Vordt of the Boreal Valley",
+        defeated = false
+    },
+    {
+        name = "Curse-Rotted Greatwood",
+        defeated = false
+    }
+}
+
 function love.load()
     love.window.setTitle("DS3 Completionist Companion")
 end
