b12d9f0ed08cfd3f4b9cc6121c22e3015a63df1b
GR <grvig06@gmail.com>
2026-06-29 11:44:13 +0530

    Dim completed items

--- main.lua
+++ main.lua
@@ -133,7 +133,11 @@ function love.draw()
         local status = boss.defeated and "[X]" or "[ ]"
         local draw_y = header_height + (i * boss_spacing) + boss_scroll
         if draw_y > header_height then
-            if i == selected_boss then love.graphics.setColor(0.9, 0.8, 0.4) end
+            if i == selected_boss then
+                love.graphics.setColor(0.9, 0.8, 0.4)
+            elseif boss.defeated then
+                love.graphics.setColor(0.35, 0.35, 0.35)
+            end
             love.graphics.print(status .. " " .. boss.name, 20, draw_y)
             love.graphics.setColor(1, 1, 1)
         end
@@ -144,7 +148,11 @@ function love.draw()
             local status = quest.completed and "[X]" or "[ ]"
             local draw_y = header_height + (i * boss_spacing) + quest_scroll
             if draw_y > header_height then
-                if i == selected_quest then love.graphics.setColor(0.9, 0.8, 0.4) end
+                if i == selected_quest then
+                    love.graphics.setColor(0.9, 0.8, 0.4)
+                elseif quest.completed then
+                    love.graphics.setColor(0.35, 0.35, 0.35)
+                end
                 love.graphics.print(status .. " " .. quest.name, mid_x + 20, draw_y)
                 love.graphics.setColor(1, 1, 1)
             end
@@ -154,7 +162,11 @@ function love.draw()
             local status = wep.obtained and "[X]" or "[ ]"
             local draw_y = header_height + (i * boss_spacing) + weapon_scroll
             if draw_y > header_height then
-                if i == selected_weapon then love.graphics.setColor(0.9, 0.8, 0.4) end
+                if i == selected_weapon then
+                    love.graphics.setColor(0.9, 0.8, 0.4)
+                elseif wep.obtained then
+                    love.graphics.setColor(0.35, 0.35, 0.35)
+                end
                 love.graphics.print(status .. " " .. wep.name, mid_x + 20, draw_y)
                 love.graphics.setColor(1, 1, 1)
             end
